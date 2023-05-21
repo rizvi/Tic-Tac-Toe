8760ed543de24613cedd65667ceec67e8c307ffc
rizvi <rizvi3389ict@gmail.com>
2023-05-21 15:56:12 +0600

    Removed unwanted eclipse class

--- src/Driver.java
+++ src/Driver.java
@@ -2,7 +2,6 @@ import com.rizvi.mit.du.console.ConsoleIO;
 import com.rizvi.mit.du.service.GameLogic;
 
 public class Driver {
-
 	public static void main(String[] args) {
 		GameLogic game = new GameLogic(new ConsoleIO());
 		game.play();
